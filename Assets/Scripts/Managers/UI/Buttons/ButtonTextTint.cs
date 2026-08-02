15c1121c67a6e3fb4e17668449d2e699504832b4
Haykal <ferramentas@haykal.com.br>
2026-08-02 09:59:47 -0300

    Update button Tint script

--- Assets/Scripts/Managers/UI/Buttons/ButtonTextTint.cs
+++ Assets/Scripts/Managers/UI/Buttons/ButtonTextTint.cs
@@ -4,35 +4,40 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
-public class ButtonTextTint : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+public class ButtonTextTint : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, ISelectHandler, IDeselectHandler
 {
     [SerializeField] private TMP_Text label;
 
+    [Header("Colors")]
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color highlightedColor = Color.white;
+    [SerializeField] private Color pressedColor = Color.white;
+    [SerializeField] private Color selectedColor = Color.white;
+    [SerializeField] private Color disabledColor = Color.white;
+    [SerializeField] private float fadeDuration = 0.1f;
+
     private Button _button;
 
-    private void Awake()
-    {
-        _button = GetComponent<Button>();
-    }
+    private void Awake() => _button = GetComponent<Button>();
 
-    public void OnPointerEnter(PointerEventData eventData) => Tint(_button.colors.highlightedColor);
-
-    public void OnPointerExit(PointerEventData eventData) => Tint(_button.colors.normalColor);
-
-    public void OnPointerDown(PointerEventData eventData) => Tint(_button.colors.pressedColor);
-
-    public void OnPointerUp(PointerEventData eventData) => Tint(_button.colors.highlightedColor);
+    private void OnEnable() => Tint(normalColor);
 
     private void OnDisable()
     {
-        if (label != null && _button != null)
+        if (label != null)
             label.CrossFadeColor(Color.white, 0f, true, true);
     }
+
+    public void OnPointerEnter(PointerEventData eventData) => Tint(highlightedColor);
+    public void OnPointerExit(PointerEventData eventData)  => Tint(normalColor);
+    public void OnPointerDown(PointerEventData eventData)  => Tint(pressedColor);
+    public void OnPointerUp(PointerEventData eventData)    => Tint(highlightedColor);
+    public void OnSelect(BaseEventData eventData)        => Tint(selectedColor);
+    public void OnDeselect(BaseEventData eventData)      => Tint(normalColor);
 
     private void Tint(Color color)
     {
         if (label == null || !_button.interactable) return;
-
-        label.CrossFadeColor(color * _button.colors.colorMultiplier, _button.colors.fadeDuration, true, true);
+        label.CrossFadeColor(color, fadeDuration, true, true);
     }
 }
