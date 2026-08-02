b1d4f88e00013feb33da14e0c7eef55406b7805f
Haykal <ferramentas@haykal.com.br>
2026-08-02 11:49:42 -0300

    Update Player Interaction

--- Assets/Scripts/Player/PlayerInteractionControl.cs
+++ Assets/Scripts/Player/PlayerInteractionControl.cs
@@ -3,11 +3,46 @@ using UnityEngine;
 
 public class PlayerInteractionControl : MonoBehaviour
 {
+    #region Debug
+
+    [SerializeField] private bool debugMode;
+
+    private void DebugMessage(string message)
+    {
+        if(debugMode)
+            Debug.Log(message);
+    }
+
+    #endregion
+    
+    [SerializeField] private KeyCode interactionButton = KeyCode.E;
+
+    private bool _isClueOnRange;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Pistas"))
         {
-            Debug.Log("pista próxima");
+            DebugMessage("pista dentro do range");
+
+            _isClueOnRange = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(interactionButton) && _isClueOnRange)
+        {
+            DebugMessage("pista maneira");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Pistas"))
+        {
+            DebugMessage("pista saiu do range");
+            _isClueOnRange = false;
         }
     }
 }
